95257bddb8e483f911335222955f271012a86d27
Alexander <sanexwell@bk.ru>
2021-09-08 15:53:03 +0300

    allow access

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.header('Access-Control-Allow-Credentials', true); // If needed
+
+  next();
+});
 app.use(compression());
 //Routes
 
