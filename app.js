427835ac5974f60b8f440a7e59a914a0bfb16adb
Alexander <sanexwell@bk.ru>
2021-09-09 12:00:44 +0300

    Authorization

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
   );
   res.header(
     'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
   ); // If needed
   res.header('Access-Control-Allow-Credentials', true); // If needed
 
