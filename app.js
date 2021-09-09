cda534f9db1b59d00b517ddb64fde1bcb2a537b0
Alexander <sanexwell@bk.ru>
2021-09-09 11:53:59 +0300

    headers cors

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ app.use(function (req, res, next) {
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  ); // If needed
   res.header('Access-Control-Allow-Credentials', true); // If needed
 
   next();
